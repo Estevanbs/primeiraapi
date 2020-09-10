c77a3208605950b29bdb42b38d95c1357de28711
Estevanbs <estevanbartmann@gmail.com>
2020-09-10 10:21:49 -0300

    pegando dados mysql

--- app.js
+++ app.js
@@ -1,59 +1,21 @@
 const express = require('express')
-
 const app = express()
+const mysql = require('mysql')
 
-var dados = [
-    {
-        "id": 1,
-        "nome": "AUTOMÓVEIS",
-        "lotes": 18
-    },
-    {
-        "id": 14,
-        "nome": "CAMINHÕES",
-        "lotes": 1
-    },
-    {
-        "id": 4,
-        "nome": "CONFECÇÕES",
-        "lotes": 4
-    },
-    {
-        "id": 2,
-        "nome": "IMÓVEIS",
-        "lotes": 54
-    },
-    {
-        "id": 12,
-        "nome": "MOTOS E MOBILETES",
-        "lotes": 4
-    },
-    {
-        "id": 3,
-        "nome": "MÁQUINAS E EQUIPAMENTOS",
-        "lotes": 24
-    },
-    {
-        "id": 5,
-        "nome": "MÓVEIS / ELETRÔNICOS",
-        "lotes": 58
-    },
-    {
-        "id": 13,
-        "nome": "OUTROS",
-        "lotes": 8
-    },
-    {
-        "id": 20,
-        "nome": "ÔNIBUS/PASSAGEIRO",
-        "lotes": 3
-    }
-]
 
-app.use(express.json())
-
-app.get("/lista", (req, res) => {
-    return res.json({dados})
+app.get("/", (req, res) => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '0000',
+        database: 'leilao'
+    })
+    connection.connect()
+    
+    connection.query("SELECT * FROM lotes", (err, rows, fields) => {
+        res.json(rows)
+    })
+    connection.end()
 })
 
 app.listen(8081, () => {
